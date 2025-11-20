96da6e85c1d6b756de2a51caf5240f10d901bd8e
FernanPC18 <231477850+FernanPC18@users.noreply.github.com>
2025-11-20 09:34:06 +0100

    feat(kata-tdd-pytest): Añadir código kata-tdd-pytest

---
+++ test/test_division_cero.py
@@ -0,0 +1,12 @@
+import pytest
+from src.raizEcuacionSegundoGrado import raiz_ecuacion_segundo_grado
+
+
+@pytest.mark.a_nulo
+def test_division_por_cero():
+    assert raiz_ecuacion_segundo_grado(0, 1, 1) == None
+
+
+@pytest.mark.a_nulo
+def test_coeficientes_nulos():
+    assert raiz_ecuacion_segundo_grado(0, 0, 0) == None
